f50dad734bb699c68041c10c144770d93e1c5946
dathen-24 <huongw135@gmail.com>
2026-06-22 01:21:55 +0700

    Update Sidebar.tsx

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -4,9 +4,9 @@ import { usePlaylistData } from "../context/PlaylistContext";
 import { useState } from "react";
 import { useSongData } from "../context/SongContext";
 
-const { songs } = useSongData();
-
 const Sidebar = () => {
+const { songs } = useSongData();
+  
   const navigate = useNavigate();
 
   const { user } = useUserData();
@@ -86,35 +86,34 @@ const Sidebar = () => {
             </div>
           ) : (
             <div className="space-y-2 overflow-y-auto">
-              {playlists.map((playlist) => (
-              const validSongCount = playlist.songs.filter(
-  (playlistSong) =>
-    songs.some(
-      (song) =>
-        String(song.id) ===
-        String(playlistSong.songId)
-    )
-).length;
-                <div
-                  key={playlist._id}
-                  onClick={() =>
-                    navigate(
-                      `/playlist/${playlist._id}`
-                    )
-                  }
-                  className="p-3 rounded-lg hover:bg-zinc-800 cursor-pointer transition"
-                >
-                  <p className="font-medium truncate">
-                    {playlist.name}
-                  </p>
+              {playlists.map((playlist) => {
+  const validSongCount = playlist.songs.filter(
+    (playlistSong) =>
+      songs.some(
+        (song) =>
+          String(song.id) ===
+          String(playlistSong.songId)
+      )
+  ).length;
 
-                  <p className="text-xs text-zinc-400">
-                    {validSongCount} songs
-                  </p>
-                </div>
-              ))}
-            </div>
-          )}
+  return (
+    <div
+      key={playlist._id}
+      onClick={() =>
+        navigate(`/playlist/${playlist._id}`)
+      }
+      className="p-3 rounded-lg hover:bg-zinc-800 cursor-pointer transition"
+    >
+      <p className="font-medium truncate">
+        {playlist.name}
+      </p>
+
+      <p className="text-xs text-zinc-400">
+        {validSongCount} songs
+      </p>
+    </div>
+  );
+})}
 
           {user?.role === "admin" && (
             <button
