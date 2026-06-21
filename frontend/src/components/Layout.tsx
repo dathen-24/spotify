2a12187144ef89cbf05b9d24841ba6294e6bed43
dathen-24 <huongw135@gmail.com>
2026-06-21 19:26:17 +0700

    Update Layout.tsx

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,33 +1,30 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Player from "./Player";
 import { Outlet } from "react-router-dom";
 
-interface LayoutProps {
-  children: ReactNode;
-}
+// Đã xóa hoàn toàn interface LayoutProps vì không còn cần thiết
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout = () => {
   return (
     <div className="h-screen bg-black/30 text-white overflow-hidden">
-      {/* Main Area */}{" "}
+      {/* Main Area */}
       <div className="h-[calc(100vh-90px)] flex gap-2 p-2">
-        {" "}
         <Sidebar />
         <main
           className="
-         flex-1
-    rounded-xl
-    overflow-y-auto
-    h-full
-    bg-gradient-to-b from-zinc-800 to-zinc-900
-      "
+            flex-1
+            rounded-xl
+            overflow-y-auto
+            h-full
+            bg-gradient-to-b from-zinc-800 to-zinc-900
+          "
         >
           <Navbar />
 
           <div className="px-6 pb-8">
-          <Outlet />  
+            <Outlet />  
           </div>
         </main>
       </div>
